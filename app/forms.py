3b4e77cb3175eb79ba9e067b0aeba9e8d4c9c5e4
Yuhei Omori <yuhei.omori@beproud.jp>
2012-08-24 15:30:22 +0900

    use twitter bootstrap 2.1.0

--- app/forms.py
+++ app/forms.py
@@ -2,13 +2,27 @@
 from django import forms
 from django.forms.formsets import formset_factory
 
+
+class KindForm(forms.Form):
+    KIND_CHOICES = (
+        ('all', u'全組み合わせ'),
+        ('pair', u'ペア構成')
+    )
+    kind = forms.ChoiceField(required=True, label=u"種別", choices=KIND_CHOICES)
+
+
 class ConditionForm(forms.Form):
-    title = forms.CharField(label=u"条件",  max_length=100, required=None)
-    variation1 = forms.CharField(label=u"選択肢1", max_length=40, required=None)
-    variation2 = forms.CharField(label=u"選択肢2", max_length=40, required=None)
-    variation3 = forms.CharField(label=u"選択肢3", max_length=40, required=None)
-    variation4 = forms.CharField(label=u"選択肢4", max_length=40, required=None)
-    variation5 = forms.CharField(label=u"選択肢5", max_length=40, required=None)
+    title = forms.CharField(label=u"条件",  max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': u'項目名'}))
+    variation1 = forms.CharField(label=u"選択肢1", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢1'}))
+    variation2 = forms.CharField(label=u"選択肢2", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢2'}))
+    variation3 = forms.CharField(label=u"選択肢3", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢3'}))
+    variation4 = forms.CharField(label=u"選択肢4", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢4'}))
+    variation5 = forms.CharField(label=u"選択肢5", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢5'}))
+    variation6 = forms.CharField(label=u"選択肢6", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢6'}))
+    variation7 = forms.CharField(label=u"選択肢7", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢7'}))
+    variation8 = forms.CharField(label=u"選択肢8", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢8'}))
+    variation9 = forms.CharField(label=u"選択肢9", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢9'}))
+    variation10 = forms.CharField(label=u"選択肢10", max_length=40, required=None, widget=forms.TextInput(attrs={'placeholder': u'選択肢10'}))
 
     def variations(self):
         return  [variation for variation in [
@@ -16,9 +30,14 @@ class ConditionForm(forms.Form):
         self.cleaned_data.get('variation2'),
         self.cleaned_data.get('variation3'),
         self.cleaned_data.get('variation4'),
-        self.cleaned_data.get('variation5')] if variation]
+        self.cleaned_data.get('variation5'),
+        self.cleaned_data.get('variation6'),
+        self.cleaned_data.get('variation7'),
+        self.cleaned_data.get('variation8'),
+        self.cleaned_data.get('variation9'),
+        self.cleaned_data.get('variation10')] if variation]
 
-ConditionFormSet = formset_factory(ConditionForm, extra=5, max_num=5)
+ConditionFormSet = formset_factory(ConditionForm, extra=10, max_num=10)
 
 
 class ActionForm(forms.Form):
--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 # coding=utf-8
-from StringIO import StringIO
+
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import itertools
 from django.template import RequestContext
 import tablib
-from app.forms import ConditionFormSet, ActionFormSet
+from app.forms import ConditionFormSet, ActionFormSet, KindForm
 
 
 def _create_sub_header_columns(name, header_length):
@@ -14,42 +15,52 @@ def _create_sub_header_columns(name, header_length):
     return tuple(condition_columns)
 
 
-def index(request):
+def render_index(action_formset, condition_formset, kind_form, request):
+    return render_to_response('index.html', RequestContext(request, {
+        'kind_form': kind_form,
+        'condition_formset': condition_formset,
+        'action_formset': action_formset
+    }))
 
+
+def index(request):
+    kind_form = KindForm(prefix='kind')
     condition_formset = ConditionFormSet(prefix='condition')
     action_formset = ActionFormSet(prefix='action')
-    return render_to_response('index.html', RequestContext(request, {
-    'condition_formset': condition_formset,
-    'action_formset': action_formset
-    }))
+    return render_index(action_formset, condition_formset, kind_form, request)
 
 
 def export(request):
 
-    condition_formset = ConditionFormSet(request.GET, prefix='condition')
-    action_formset = ActionFormSet(request.GET, prefix='action')
+    kind_form = KindForm(request.POST, prefix="kind")
+    condition_formset = ConditionFormSet(request.POST, prefix='condition')
+    action_formset = ActionFormSet(request.POST, prefix='action')
 
-    if all([formset.is_valid() for formset in [condition_formset,  action_formset]]):
+    if all([formset.is_valid() for formset in [condition_formset, action_formset, kind_form]]):
 
-        variation_counts = [len(form.variations()) for form in condition_formset if form.variations()]
+        if kind_form.cleaned_data["kind"] == 'all':
+            variations = list(itertools.product(*[form.variations() for form in condition_formset if form.variations()]))
 
-        rule_count = 1
-        for variation_count in variation_counts:
-            rule_count = rule_count * variation_count
+        else:
+            import metacomm.combinatorics.all_pairs2
+            all_pairs = metacomm.combinatorics.all_pairs2.all_pairs2
+            variations = list(all_pairs([form.variations() for form in condition_formset if form.variations()]))
+
+        # ヘッダ
+        rule_count = len(variations)
+        if (rule_count > 255):
+            messages.error(request, '条件の組み合わせが多すぎます。全組み合わせを選択している場合はペア構成で試してみてください。')
+            return render_index(action_formset, condition_formset, kind_form, request)
 
         headers = [u"ルール%s" % i for i in range(1, rule_count+1)]
         headers.insert(0, '')
         header_length = len(headers)
-
         data = tablib.Dataset(headers=tuple(headers))
-        variations = list(itertools.product(*[form.variations() for form in condition_formset if form.variations()]))
 
         # 条件
         data.append(_create_sub_header_columns(u'条件', header_length))
         for i, condition_form in enumerate(condition_formset):
-
             if condition_form.variations():
-
                 row = [ "    " + condition_form.cleaned_data['title']]
                 for variation in variations:
                     row.append(variation[i])
@@ -59,10 +70,10 @@ def export(request):
         data.append(_create_sub_header_columns(u'アクション', header_length))
         for action_form in action_formset:
             if action_form.cleaned_data.get('title'):
-
                 action_columns = [None] * header_length
                 action_columns[0] = "    " + action_form.cleaned_data['title']
                 data.append(tuple(action_columns))
+
 
         response = HttpResponse(data.xls,
                                 mimetype='application/vnd.ms-excel; charset=utf-8')
@@ -71,7 +82,6 @@ def export(request):
         return response
 
     else:
-        return render_to_response('index.html', RequestContext(request, {
-            'condition_formset': condition_formset,
-            'action_formset': action_formset
-        }))
+        return render_index(action_formset, condition_formset, kind_form, request)
+
+
